49eb0477ce451eb0bb01d80474c2d93446aab7bd
Ted Szylowiec <tedszy@gmail.com>
2015-08-29 12:48:11 +0700

    Changed ^M to newlines in sample.csv.

--- document_test.cc
+++ document_test.cc
@@ -1,9 +1,14 @@
-#include <sstream>
+#include <fstream>
 #include "document.h"
 
 int main ()
 {
-  std::cout << "ok" << std::endl;
+  std::ifstream my_file;
+  Document_parser dp;
+  my_file.open("sample.csv");
+  my_file >> dp;
+  my_file.close();
+  std::cout << dp << std::endl;
 
   return 0;
 }
