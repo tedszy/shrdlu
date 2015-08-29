632074c5410a6eef48114c25b7f0e34026fccf4b
Ted Szylowiec <tedszy@gmail.com>
2015-08-29 15:35:18 +0700

    Removed foo, testing.cc, testing.h

--- testing.h
+++
@@ -1,8 +0,0 @@
-#include <sstream>
-#include <vector>
-
-void check_record(std::vector<std::string> a, std::vector<std::string> b, std::string msg);
-void check_field(std::string, std::string, std::string);
-void check_fail(std::stringstream&, std::string);
-void check_pass(std::stringstream&, std::string);
-void check_true(bool, std::string);
